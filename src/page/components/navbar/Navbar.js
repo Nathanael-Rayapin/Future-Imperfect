33f4f63c564a391120c88de3d0c488a42b7fa123
Nathanael421 <nathanael.rayapin@gmail.com>
2023-01-25 21:01:29 +0100

    Navbar implemented

---
+++ src/page/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar-container d-flex justify-content-between py-0">
+      <div className="brand d-flex align-items-center p-4">
+        <h1 className="mb-0">Future Imperfect</h1>
+      </div>
+      <div className="icons d-flex align-items-center">
+        <div className="search-icon py-3 px-4">
+          <i className="bi bi-search"></i>
+        </div>
+        <div className="burger-menu-icon py-3 px-4">
+          <i className="bi bi-list"></i>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
