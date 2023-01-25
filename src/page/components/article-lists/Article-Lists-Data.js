dcb64c179f27c0945fbcab9d07ca8d0a6b11c9db
Nathanael421 <nathanael.rayapin@gmail.com>
2023-01-25 21:02:27 +0100

    Articles Data Added

---
+++ src/page/components/article-lists/Article-Lists-Data.js
@@ -0,0 +1,102 @@
+export const ARTICLES_DATAS = [
+  {
+    id: 1,
+    title: "Le dragon de Komodo",
+    sub_title:
+      "Le dragon de Komodo (Varanus komodoensis), le plus grand lézard du monde.",
+    date: "25 Janvier 2015",
+    author: "Corina M. Bacon",
+    avatar:
+      "https://images.unsplash.com/photo-1588516903720-8ceb67f9ef84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=744&q=80",
+    cover: "./assets/img/dragon-komodo.jpg",
+    content: `En France, beaucoup de personnes apprécient de voir les petits lézards filer à toute vitesse à notre approche. Lorsqu’on les aperçoit, c’est qu’ils sont 
+    sortis profiter de la chaleur du soleil. Ils sont donc associés au beau temps. Mais le dragon du Komodo nous fait pénétrer dans un tout autre univers.
+    Ce géant mesure en moyenne 2,59 mètres de long et pèse entre 79 et 91 kg. Sa mâchoire, moins puissante que celle du crocodile, ne présente pas moins d’une soixantaine
+    de dents qui peuvent mesurer 2,5 cm de longueur. Comme ses mâchoires sont reliées par un ligament très élastique, il peut ouvrir très largement sa gueule, ce qui est
+    très impressionnant. Une caractéristique contribue à accentuer son allure effrayante : sa salive est souvent teintée de son propre sang car ses dents sont recouvertes
+    d’un tissu qui se déchire lorsque l'animal s'alimente.
+    Cet animal est équipé de glandes à venin placées, non pas au-dessus de la mâchoire comme chez les serpents, mais en dessous. Ce venin provoque une forte chute de 
+    pression artérielle et, à ce jour, il n’existe aucun antidote. Toutefois, si la plaie est nettoyée immédiatement et que des antibiotiques en forte quantité sont 
+    administrés, il est possible d’annuler son action. Sans cela, seule l’amputation du membre touché permet de sauver la vie de la victime.
+    Découvert par les scientifiques en 1910, sa réputation a toujours été celle d'un animal redoutable. Les dragons de Komodo ont longtemps été populaires dans les zoos 
+    : on faisait entrer les gens dans les enclos pour qu’ils les approchent de plus près. Finalement, même si ses attaques ne sont pas prévisibles, il a tué peu d'êtres 
+    humains. Aujourd’hui, son aire de distribution est bien réduite et l'UICN (Union internationale pour la conservation de la nature) l'a classé comme espèce menacée 
+    et il bénéficie d’une protection.`,
+    type: "terrestre",
+    likes: 1200,
+    comments: 0,
+  },
+  {
+    id: 2,
+    title: "L’hippopotame",
+    sub_title:
+      "L’hippopotame (hippopotamus), troisième plus grand mammifère du monde après l’éléphant et le rhinocéros",
+    date: "15 Novembre 2022",
+    author: "Kelly A. Pereira",
+    avatar:
+      "https://images.unsplash.com/photo-1593575391244-2f16fcf4cbf8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    cover: "./assets/img/hipopotame.jpg",
+    content: `Responsable de nombreuses morts humaines (environ 300 par an) sur le continent africain, cela fait de lui un animal très craint des personnes qui vivent à proximité. 
+    C’est un animal très territorial qui défend l’espace sur lequel il vit avec force.
+    Les mâles mesurent 1,50 mètre au garrot et dépassent les trois mètres de long. Leur poids oscille entre 2,5 et 3,5 tonnes, les plus gros pouvant atteindre les 4,5 
+    tonnes. Il est extrêmement agile sous l'eau et atteint sur terre des vitesses de pointes avoisinant les 45 km/h. Compte tenu du peu de chances d’échapper à cet 
+    agressif poids lourd, il vaut mieux ne pas s'approcher trop près.`,
+    type: "terrestre",
+    likes: 986,
+    comments: 0,
+  },
+  {
+    id: 3,
+    title: "Le jaguar",
+    sub_title: "Le jaguar (Panthera onca) , le plus grand félin d'Amérique",
+    date: "09 Septembre 2022",
+    author: "John M. Dupree",
+    avatar:
+      "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    cover: "./assets/img/jaguar.jpg",
+    content: `Le jaguar est connu pour être capable de sortir de l’eau un caïman de 70 kg et le rabattre sur la berge. C’est que cet animal possède une mâchoire exceptionnellement 
+    puissante, la plus puissante des grands félins d’ailleurs, en proportion de sa taille. Le poids d’un mâle varie entre 56 et 102 kg pour une longueur comprise entre 
+    1,4 et 1,85 mètre. Ses mâchoires sont courtes, actionnées par de gros muscles. Rapide, il cible le cerveau ou la colonne vertébrale pour paralyser sa proie : un seul 
+    coup de croc peut alors suffire à tuer sa cible qui peut peser jusqu’à 300 kg.`,
+    type: "terrestre",
+    likes: 1054,
+    comments: 0,
+  },
+  {
+    id: 4,
+    title: "Le grand buffle noir des savanes",
+    sub_title:
+      "Le grand buffle noir des savanes (Syncerus caffer), figure parmi les représentants du fameux groupe des Big five",
+    date: "30 Juillet, 2022",
+    author: "Paul A. Woodyard",
+    avatar:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    cover: "./assets/img/buffle.jpg",
+    content: `Le grand buffle noir des savanes africaines (Syncerus caffer) peut atteindre 1,7 mètre de hauteur et 3,4 m en longueur pour 900 kg. En raison de sa dangerosité et de sa 
+    couleur, il a été surnommé la « peste noire ». Les bufflonnes, un peu plus légères, peuvent atteindre les 55 km/h de pointe. La tête de ce buffle est équipée d'une paire 
+    de cornes épaisses et courbées vers le haut. Elles fusionnent à leur base, formant comme un casque très protecteur. Leur envergure oscille entre 1 mètre et 1,50 mètre. 
+    Outre ce corps puissant, la dangerosité du buffle est accentuée par son caractère imprévisible.`,
+    type: "terrestre",
+    likes: 779,
+    comments: 0,
+  },
+  {
+    id: 5,
+    title: "La grenouille à flèches empoisonnées",
+    sub_title:
+      "La grenouille à flèches empoisonnées (Phyllobates terribilis), la grenouille à flèches empoisonnées considérée comme la plus dangereuse",
+    date: "02 Juin 2022",
+    author: "Cecelia F. Llanes",
+    avatar:
+      "https://images.unsplash.com/photo-1606752445013-eeb8cc06aabd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    cover: "./assets/img/frog.jpg",
+    content: `De couleur bleutée, on la trouve au Nicaragua et au Brésil. C’est l’une des plus grandes espèces de Dendrobatidae avec ses seulement 4,7 cm en moyenne. Elle 
+    vit dans des forêts tropicales humides où la température est d'au moins 25 °C. En guise de mécanisme de défense, sa peau sécrète un puissant poison, mortel pour ses proies. 
+    Les glandes de leur peau produisent de la batrachotoxine, substance identifiée comme étant parmi les plus toxiques au monde. En cas d’ingestion, elle agit sur les nerfs et 
+    les muscles et peut entraîner insuffisance cardiaque ou fibrillation. Chez l’être humain, seul un contact accidentel peut être craint et il ne causera que des sensations de 
+    brûlure durables.`,
+    type: "terrestre",
+    likes: 881,
+    comments: 0,
+  },
+];
